b7f2d08367552300e6be58f51df30f95bf44839d
Martin Pärtel <martin.partel@gmail.com>
2012-10-10 23:20:42 +0300

    rsync latest.img.gz after main rsync is done.

--- rsync.sh
+++ rsync.sh
@@ -7,4 +7,5 @@ if [ -z "$RSYNC_DEST" ]; then
     echo "RSYNC_DEST is not set. Set it in settings.sh or in the env."
 fi
 
-rsync -avv --chmod=a+rX --progress --exclude=current.img images/ $RSYNC_DEST
+rsync -avv --chmod=a+rX --progress --exclude=current.img --exclude latest.img.gz images/ $RSYNC_DEST
+rsync -avv --chmod=a+rX images/latest.img.gz $RSYNC_DEST/
