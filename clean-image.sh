229376904ffa4fe131e90cc8af9bd5ea8be4b52c
Martin Pärtel <martin.partel@gmail.com>
2014-01-28 02:28:28 +0200

    Clean .tmcbeans instead of .netbeans.

--- clean-image.sh
+++ clean-image.sh
@@ -14,7 +14,7 @@ for homedir in "$ROOT"/home/* "$ROOT"/root; do
     rm -f .bash_history
     rm -f .local/share/recently-used.xbel
     rm -Rf .cache
-    
+
     if [ -d .mozilla/firefox ]; then
         for profile in .mozilla/firefox/*.default; do
             rm -Rf $profile/*Cache $profile/minidumps $profile/cookies.* $profile/formhistory.* $profile/downloads.*
@@ -24,12 +24,13 @@ for homedir in "$ROOT"/home/* "$ROOT"/root; do
             fi
         done
     fi
-    
-    if [ -d .netbeans/* ]; then
-        rm -Rf .netbeans/*/config/tmc
-        rm -f .netbeans/*/config/Preferences/fi/helsinki/cs/tmc.properties
+
+    NBDIR=".tmcbeans"
+    if [ -d $NBDIR/* ]; then
+        rm -Rf $NBDIR/*/config/tmc
+        rm -f $NBDIR/*/config/Preferences/fi/helsinki/cs/tmc.properties
     fi
-    
+
     cd "$OLDPWD"
 done
 
